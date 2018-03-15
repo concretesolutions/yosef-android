b51ec1694a6494795a30e872ccf9e3e7cf139cce
yasmin <yasmin.pereira@concrete.com.br>
2018-03-15 15:26:41 -0300

    Use static imports for espresso methods

--- canarinho/src/androidTest/kotlin/br/com/concrete/yosef/canarinho/component/CanarinhoComponentTextField.kt
+++ canarinho/src/androidTest/kotlin/br/com/concrete/yosef/canarinho/component/CanarinhoComponentTextField.kt
@@ -1,20 +1,25 @@
 package br.com.concrete.yosef.canarinho.component
 
 import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.widget.EditText
 import br.com.concrete.yosef.canarinho.activities.TextFieldActivity
+import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.Matchers
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class CanarinhoComponentText {
+class CanarinhoComponentTextField {
 
     @Rule
     @JvmField
@@ -22,12 +27,12 @@ class CanarinhoComponentText {
 
     @Test
     fun wheAddTextWithMonetaryMaskThenShowValueFormatted() {
-        Espresso.onView(Matchers.allOf(
-                ViewMatchers.isAssignableFrom(EditText::class.java)))
-                .perform(ViewActions.typeText("123123123"))
+        onView(allOf(
+                isAssignableFrom(EditText::class.java)))
+                .perform(typeText("123123123"))
 
-        Espresso.onView(ViewMatchers.withText("R$ 1.231.231,23"))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withText("R$ 1.231.231,23"))
+                .check(matches(isDisplayed()))
     }
 
 }
