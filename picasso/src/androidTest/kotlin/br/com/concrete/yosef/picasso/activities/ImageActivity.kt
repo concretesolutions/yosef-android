367b2d3287b837b6ac56c8009088ad3d89664c7a
Matheus Cassiano Candido <matheus@mcassiano.com>
2018-03-23 19:08:21 -0300

    Fixing tests for other modules

--- picasso/src/androidTest/kotlin/br/com/concrete/yosef/picasso/activities/ImageActivity.kt
+++ picasso/src/androidTest/kotlin/br/com/concrete/yosef/picasso/activities/ImageActivity.kt
@@ -12,7 +12,6 @@ class ImageActivity : AppCompatActivity(), OnActionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val json = ImageActivity::class.java.getResource("/assets/example_image.json").readText()
 
@@ -20,7 +19,7 @@ class ImageActivity : AppCompatActivity(), OnActionListener {
             .addComponentFor(PicassoImageComponent.IMAGE_TYPE, PicassoImageComponent())
             .build()
 
-        dynamicView.createViewFromJson(findViewById(R.id.parent), json, this)
+        setContentView(dynamicView.createViewFromJson(this, json, this))
     }
 
     override fun callAction(value: String) {
