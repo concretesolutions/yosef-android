286c9ccf9bd4a9ae85763ea57ea265619933575e
Rodrigo Caetano <rdg.mnz@gmail.com>
2018-05-10 18:25:57 -0300

    Add backgroound color to image components

--- glide/src/main/kotlin/br/com/concrete/yosef/glide/GlideImageComponent.kt
+++ glide/src/main/kotlin/br/com/concrete/yosef/glide/GlideImageComponent.kt
@@ -8,6 +8,8 @@ import android.widget.ImageView
 import br.com.concrete.yosef.OnActionListener
 import br.com.concrete.yosef.api.component.Component
 import br.com.concrete.yosef.api.property.DynamicPropertyCommand
+import br.com.concrete.yosef.api.property.color.BackgroundColorCommand
+import br.com.concrete.yosef.api.property.color.BackgroundColorCommand.Companion.BACKGROUND_COLOR
 import br.com.concrete.yosef.api.property.id.IdCommand
 import br.com.concrete.yosef.api.property.id.IdCommand.Companion.ID
 import br.com.concrete.yosef.api.property.size.AspectRatioCommand
@@ -36,6 +38,7 @@ class GlideImageComponent(
         SCALE_TYPE to ScaleTypePropertyCommand(),
         IMAGE_URL to ImageUrlCommand(glide),
         ASPECT_RATIO to AspectRatioCommand(),
+        BACKGROUND_COLOR to BackgroundColorCommand(),
         ID to IdCommand()
     )
 
